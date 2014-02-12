f8ef8b4a9730dff15a935b31caa9f2f5f79b32e8
Masato Ikeda <masato.ikeda@gmail.com>
2014-02-12 13:56:35 +0900

    Implement `ec2_tagged` method

--- lib/capistrano/ec2_tagged.rb
+++ lib/capistrano/ec2_tagged.rb
@@ -1,7 +1,27 @@
 require "capistrano/ec2_tagged/version"
+require "aws-sdk"
 
 module Capistrano
   module Ec2Tagged
-    # Your code goes here...
+    def ec2_tagged(keys_and_values = {})
+      @ec2 ||= AWS::EC2.new(
+        :access_key_id      => fetch(:access_key_id),
+        :secret_access_key  => fetch(:secret_access_key),
+        :region             => fetch(:region)
+      )
+
+      instances = @ec2.instances
+
+      keys_and_values.each do |key, value|
+        instances = instances.tagged(key.to_s)
+        instances = instances.tagged_values(value.to_s) if value != true
+      end
+
+      instances.map { |instance|
+        instance.ip_address || instance.private_ip_address if instance.status == :running
+      }.compact
+    end
   end
 end
+
+extend Capistrano::Ec2Tagged
